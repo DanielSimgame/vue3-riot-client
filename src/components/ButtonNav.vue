30687744cad5d0dcc2372d60a2f101b2a70e6a9a
Daniel Morrison <1013921026@qq.com>
2021-10-07 14:58:15 +0800

    Rudimentary Navigation Bar

---
+++ src/components/ButtonNav.vue
@@ -0,0 +1,28 @@
+<template>
+  <router-link :to="$props.link">
+    <div class="rounded-2xl w-12 h-12 bg-riotGray-light
+         hover:bg-riotGray-lightest active:bg-riotGray-light bg-opacity-75 flex justify-center items-center">
+      <p class="text-white">{{ $props.content }}</p>
+    </div>
+  </router-link>
+</template>
+
+<script>
+export default {
+  name: "ButtonNav",
+  props: {
+    link: {
+      type: String,
+      default: "/"
+    },
+    content: {
+      type: String,
+      default: "田",
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/components/UniversalHeader.vue
@@ -0,0 +1,33 @@
+<template>
+  <header class="bg-riotGray shadow">
+    <div class="px-4 py-6 mx-auto max-w-7xl sm:px-6 lg:px-8 flex flex-row justify-between">
+      <ButtonNav link="/about" content="田" ></ButtonNav>
+      <div class="">
+        <h1
+            class="text-white text-3xl font-bold leading-tight text-gray-900">
+          {{ title }}
+        </h1>
+      </div>
+      <ButtonNav link="/about" content="人" ></ButtonNav>
+    </div>
+  </header>
+</template>
+
+<script>
+import {defineComponent} from "vue";
+import ButtonNav from "@/components/ButtonNav.vue";
+
+export default defineComponent({
+  name: "UniversalHeader",
+  components: {ButtonNav},
+  data() {
+    return {
+      title: "KADaniel"
+    }
+  },
+})
+</script>
+
+<style scoped>
+
+</style>
