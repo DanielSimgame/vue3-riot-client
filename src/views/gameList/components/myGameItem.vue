478cec51489099209a259b02fd3df900b9924a9b
Daniel Morrison <1013921026@qq.com>
2021-10-13 22:03:37 +0800

    myGameItem.vue effect done

--- src/views/gameList/components/myGameItem.vue
+++ src/views/gameList/components/myGameItem.vue
@@ -1,21 +1,22 @@
 <template>
-  <div class="game-list-item box-content relative group">
-    <div class="child-bg absolute bg-transparent transition group-hover:animate-gameItemHover_bgcolor w-80 h-60" style="left: -10px;top: -10px;z-index: 0"></div>
+  <div class="game-list-item box-content relative">
+<!--    <div class="child-bg absolute bg-transparent transition"-->
+<!--         style="left: -10px;top: -10px;z-index: 0"></div>-->
     <div
-        class="hoverBox p-1 rounded-xl transform transition duration-100 ease-in-out border-4 border-riotGray border-solid
-              group-hover:animate-gameItemHover_scale animate-rotate group-hover:border-4 group-hover:border-riotGray-lightest group-hover:border-solid"
-        style="z-index: 1;background-color: #1a232a;
-        background-repeat: no-repeat;
-        background-position: 0 0;
-        background-image: conic-gradient(transparent, rgba(168, 239, 255, 1), transparent 30%);box-sizing: border-box;">
-      <router-link :to="$props.gameLink">
-        <img class="" :src="$props.gameImage" alt/>
-      </router-link>
+        class="hoverBox rounded-xl transform transition duration-100 ease-in-out border-riotGray border-solid
+              hover:animate-gameItemHover_scale">
+      <div class="hoverBox-inner">
+        <router-link :to="$props.gameLink">
+          <img class="" :src="$props.gameImage" alt/>
+        </router-link>
+      </div>
     </div>
     <div class="myGameBox px-5 flex flex-row" style="z-index: 2">
-<!--      <img class="" v-if="$props.iconType === 1" :src="$props.gameIcon" alt="" height="50" width="50">-->
-      <font-awesome-icon v-if="$props.iconType === 1" class="text-white text-4xl my-auto mx-1.5" :icon="['fas', 'fist-raised']" />
-      <font-awesome-icon v-else-if="$props.iconType === 2" class="text-white text-4xl my-auto mx-1.5" :icon="['fas', 'gamepad']" />
+      <!--      <img class="" v-if="$props.iconType === 1" :src="$props.gameIcon" alt="" height="50" width="50">-->
+      <font-awesome-icon v-if="$props.iconType === 1" class="text-white text-4xl my-auto mx-1.5"
+                         :icon="['fas', 'fist-raised']"/>
+      <font-awesome-icon v-else-if="$props.iconType === 2" class="text-white text-4xl my-auto mx-1.5"
+                         :icon="['fas', 'gamepad']"/>
       <p class="game-title text-white font-light text-left py-2">{{ $props.gameTitle }}</p>
     </div>
   </div>
@@ -57,5 +58,57 @@ export default {
 </script>
 
 <style scoped>
+.hoverBox {
+  position: relative;
+  /*padding有关边框厚度*/
+  padding: 4px;
+  z-index: 1;
+  /*width: 400px;*/
+  /*height: 300px;*/
+  /*margin: 20px;*/
+  border-radius: 10px;
+  overflow: hidden;
+  box-sizing: content-box;
+  background-clip: content-box;
+  /*padding: 2rem;*/
+}
 
+.hoverBox::before {
+
+  content: '';
+  position: absolute;
+  z-index: -2;
+  left: -50%;
+  top: -50%;
+  width: 200%;
+  height: 200%;
+  background-color: transparent;
+  background-repeat: no-repeat;
+  background-position: 0 0;
+  background-image: none;
+  animation: rotate 4s linear infinite;
+}
+.hoverBox:hover::before {
+  background-color: #4A4A4A;
+  background-image: conic-gradient(transparent, rgba(168, 239, 255, 1), transparent 30%);
+}
+
+.hoverBox::after {
+  content: '';
+  position: absolute;
+  z-index: -1;
+  /*上左边框宽度*/
+  left: 4px;
+  top: 4px;
+  /*下右边框宽度*/
+  width: calc(100% - 8px);
+  height: calc(100% - 8px);
+  background: #1C1C1C;
+  border-radius: 4px;
+}
+
+.hoverBox-inner{
+  border-radius: 5px;
+  border: #1c1c1c 5px solid;
+}
 </style>
