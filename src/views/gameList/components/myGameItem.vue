b5a1d7bb618b8ec44a63791a4f6db9351b257d9b
Daniel Morrison <kraudaniel@vip.qq.com>
2021-10-14 12:53:54 +0800

    hover effect with error

--- src/views/gameList/components/myGameItem.vue
+++ src/views/gameList/components/myGameItem.vue
@@ -1,10 +1,14 @@
 <template>
   <div class="game-list-item box-content relative">
-<!--    <div class="child-bg absolute bg-transparent transition"-->
-<!--         style="left: -10px;top: -10px;z-index: 0"></div>-->
+    <!--    <div class="child-bg absolute bg-transparent transition"-->
+    <!--         style="left: -10px;top: -10px;z-index: 0"></div>-->
     <div
         class="hoverBox rounded-xl transform transition duration-100 ease-in-out border-riotGray border-solid
-              hover:animate-gameItemHover_scale">
+              hover:animate-gameItemHover_scale"
+        ref="hoverBox"
+        @mouseenter="onMouseEnterHandler"
+        @mousemove="onMouseMoveHandler"
+        @mouseleave="onMouseLeaveHandler">
       <div class="hoverBox-inner">
         <router-link :to="$props.gameLink">
           <img class="" :src="$props.gameImage" alt/>
@@ -30,7 +34,9 @@ export default {
   name: "myGameItem",
   data() {
     return {
-      riotIcon: defaultIcon
+      riotIcon: defaultIcon,
+      counter: 0,
+      updateRate: 0,
     }
   },
   props: {
@@ -53,6 +59,109 @@ export default {
       type: String,
       default: "/myGame"
     }
+  },
+  mounted() {
+    let _hoverBox = this.$refs.hoverBox
+    mouse.setOrigin(this.$refs.hoverBox)
+  },
+  methods: {
+    /**
+     * 根据鼠标移动的距离来改变 .hoverBox 的background旋转角度。
+     * @param event 鼠标事件
+     * */
+    update: (event) => {
+      let _self = this
+      this.$nextTick(() => {
+        let _hoverBox = _self.$refs.hoverBox
+        mouse.updatePosition(event)
+        _self.$options.methods.updateTransformStyle(
+            (mouse.y / _hoverBox.offsetHeight / 2).toFixed(2),
+            (mouse.x / _hoverBox.offsetWidth / 2).toFixed(2)
+        )
+      })
+    },
+    /**
+     * 更新元素样式的 transform 属性。
+     * @param x 坐标x
+     * @param y 坐标y
+     * */
+    updateTransformStyle: (x, y) => {
+      let style = "rotateX(" + x + "deg) rotateY(" + y + "deg)"
+      this.$nextTick(() => {
+        let _hoverBox = this.$refs.hoverBox
+        _hoverBox.style.transform = style
+        _hoverBox.style.webkitTransform = style
+        _hoverBox.style.mozTransform = style
+        _hoverBox.style.msTransform = style
+        _hoverBox.style.oTransform = style
+      })
+    },
+    /**
+     * 控制 update() 执行次数，只有当其的返回值为 true 时才执行update()。
+     * 为减少 update() 执行次数，以提高代码性能。
+     * 当 counter 值是 updateRate 整数倍时，更新才发生。
+     * 表示 isTimeToUpdate() 每执行 10 次，更新才发生一次。
+     * */
+    isTimeToUpdate: () => {
+      return this.$data.counter++ % this.$data.updateRate === 0
+    },
+    /**
+     * 处理鼠标进入事件。
+     * 鼠标进入时使.hoverBox的背景旋转。
+     * @param event 鼠标事件
+     * */
+    onMouseEnterHandler: (event) => {
+      this.$options.methods.update(event)
+    },
+    /**
+     * 处理鼠标移动事件。
+     * 当鼠标移动时改变.hoverBox背景旋转角度。
+     * @param event 鼠标事件
+     * */
+    onMouseMoveHandler: (event) => {
+      if (this.$options.methods.isTimeToUpdate()) {
+        this.$options.methods.update(event)
+      }
+    },
+    /**
+     * 处理鼠标离开事件。
+     * 鼠标离开时，重置.hoverBox样式与$data中counter, updateRate值。
+     * */
+    onMouseLeaveHandler: () => {
+      this.$nextTick(() => {
+        this.$refs.hoverBox.style = null
+      })
+    }
+  }
+}
+
+let mouse = {
+  _x: 0,
+  _y: 0,
+  x: 0,
+  y: 0,
+  /**
+   * 获取鼠标当前相对(0,0)的坐标位置。
+   * @param event 鼠标事件
+   * */
+  updatePosition: (event) => {
+    let e = event || Event
+    this.x = e.clientX - this._x
+    this.y = (e.clientY - this._y) * -1
+  },
+  /**
+   * 设置鼠标的初始坐标，即把.hoverBox的中心位置设为(0,0)。
+   * @param dom dom元素
+   * */
+  setOrigin: (dom) => {
+    this._x = dom.offsetLeft + Math.floor(dom.offsetWidth / 2)
+    this._y = dom.offsetTop + Math.floor(dom.offsetHeight / 2)
+  },
+  /**
+   * 用来展示鼠标当前位置，仅作测试用。
+   * */
+  show: () => {
+    return '(' + this.x + ', ' + this.y + ')'
   }
 }
 </script>
@@ -86,8 +195,9 @@ export default {
   background-repeat: no-repeat;
   background-position: 0 0;
   background-image: none;
-  animation: rotate 4s linear infinite;
+  /*animation: rotate 4s linear infinite;*/
 }
+
 .hoverBox:hover::before {
   background-color: #4A4A4A;
   background-image: conic-gradient(transparent, rgba(168, 239, 255, 1), transparent 30%);
@@ -107,7 +217,7 @@ export default {
   border-radius: 4px;
 }
 
-.hoverBox-inner{
+.hoverBox-inner {
   border-radius: 5px;
   border: #1c1c1c 5px solid;
 }
